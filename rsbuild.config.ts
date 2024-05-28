9e3684daa0c1a7ae373a17ecc3a02814e0744ede
SoonIter <sooniter@gmail.com>
2024-05-28 10:21:42 +0800

    chore: init

---
+++ rsbuild.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  dev: {
+    writeToDisk: true
+  },
+  tools: {
+    rspack(config) {
+      // @ts-ignore
+      config.optimization.mangleExports = false
+      // @ts-ignore
+      config.optimization.providedExports = false
+    }
+  }
+});
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+// import './App.css';
+import {
+  SunOne,
+  STurnDown,
+  Sailboat,
+  Sagittarius,
+  Sapling,
+  Sunset,
+  // ACane,
+  // Abdominal
+} from '@icon-park/react';
+
+import { useCallback } from 'react';
+console.log(Sunset,11111);
+// console.log(ACane);
+// console.log(useCallback);
+// console.log(useCallback);
+// console.log(useCallback);
+// console.log(useCallback);
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Rsbuild with React1112</h1>
+      <p>11111 building amazing things with Rsbuild.1111111111asdsa</p>
+    </div>
+  );
+};
+
+export default App;
